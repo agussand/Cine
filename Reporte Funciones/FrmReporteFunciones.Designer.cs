442d2d22fb16ccea3a4adefbc2feba22a654246d
Agustin Sandoval <119716131+agussand@users.noreply.github.com>
2023-11-21 19:58:34 -0300

    Reportes

---
+++ Reporte Funciones/FrmReporteFunciones.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Reporte_Funciones
+{
+    partial class FrmReporteFunciones
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            this.sPOBTENERFUNCIONFECHASBindingSource = new System.Windows.Forms.BindingSource();
+            this.dSFunciones = new Reporte_Funciones.DSFunciones();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.sP_OBTENER_FUNCION_FECHASTableAdapter = new Reporte_Funciones.DSFuncionesTableAdapters.SP_OBTENER_FUNCION_FECHASTableAdapter();
+            this.dtpFechaDesde = new System.Windows.Forms.DateTimePicker();
+            this.dtpFechaHasta = new System.Windows.Forms.DateTimePicker();
+            this.btnGenerar = new System.Windows.Forms.Button();
+            this.lblDesde = new System.Windows.Forms.Label();
+            this.lblHasta = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.sPOBTENERFUNCIONFECHASBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dSFunciones)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // sPOBTENERFUNCIONFECHASBindingSource
+            // 
+            this.sPOBTENERFUNCIONFECHASBindingSource.DataMember = "SP_OBTENER_FUNCION_FECHAS";
+            this.sPOBTENERFUNCIONFECHASBindingSource.DataSource = this.dSFunciones;
+            // 
+            // dSFunciones
+            // 
+            this.dSFunciones.DataSetName = "DSFunciones";
+            this.dSFunciones.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // reportViewer1
+            // 
+            reportDataSource1.Name = "DataSet1";
+            reportDataSource1.Value = this.sPOBTENERFUNCIONFECHASBindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "Reporte_Funciones.rFunciones.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(28, 98);
+            this.reportViewer1.Margin = new System.Windows.Forms.Padding(4);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.ServerReport.BearerToken = null;
+            this.reportViewer1.Size = new System.Drawing.Size(890, 427);
+            this.reportViewer1.TabIndex = 0;
+            // 
+            // sP_OBTENER_FUNCION_FECHASTableAdapter
+            // 
+            this.sP_OBTENER_FUNCION_FECHASTableAdapter.ClearBeforeFill = true;
+            // 
+            // dtpFechaDesde
+            // 
+            this.dtpFechaDesde.Location = new System.Drawing.Point(109, 48);
+            this.dtpFechaDesde.Margin = new System.Windows.Forms.Padding(4);
+            this.dtpFechaDesde.Name = "dtpFechaDesde";
+            this.dtpFechaDesde.Size = new System.Drawing.Size(265, 22);
+            this.dtpFechaDesde.TabIndex = 1;
+            // 
+            // dtpFechaHasta
+            // 
+            this.dtpFechaHasta.Location = new System.Drawing.Point(509, 48);
+            this.dtpFechaHasta.Margin = new System.Windows.Forms.Padding(4);
+            this.dtpFechaHasta.Name = "dtpFechaHasta";
+            this.dtpFechaHasta.Size = new System.Drawing.Size(256, 22);
+            this.dtpFechaHasta.TabIndex = 2;
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.Location = new System.Drawing.Point(813, 43);
+            this.btnGenerar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(105, 30);
+            this.btnGenerar.TabIndex = 3;
+            this.btnGenerar.Text = "Generar";
+            this.btnGenerar.UseVisualStyleBackColor = true;
+            this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click);
+            // 
+            // lblDesde
+            // 
+            this.lblDesde.AutoSize = true;
+            this.lblDesde.Location = new System.Drawing.Point(25, 48);
+            this.lblDesde.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblDesde.Name = "lblDesde";
+            this.lblDesde.Size = new System.Drawing.Size(48, 16);
+            this.lblDesde.TabIndex = 4;
+            this.lblDesde.Text = "Desde";
+            // 
+            // lblHasta
+            // 
+            this.lblHasta.AutoSize = true;
+            this.lblHasta.Location = new System.Drawing.Point(434, 50);
+            this.lblHasta.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblHasta.Name = "lblHasta";
+            this.lblHasta.Size = new System.Drawing.Size(43, 16);
+            this.lblHasta.TabIndex = 5;
+            this.lblHasta.Text = "Hasta";
+            // 
+            // FrmReporteFunciones
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(947, 554);
+            this.Controls.Add(this.lblHasta);
+            this.Controls.Add(this.lblDesde);
+            this.Controls.Add(this.btnGenerar);
+            this.Controls.Add(this.dtpFechaHasta);
+            this.Controls.Add(this.dtpFechaDesde);
+            this.Controls.Add(this.reportViewer1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "FrmReporteFunciones";
+            this.Text = "Reporte Funciones";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.sPOBTENERFUNCIONFECHASBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dSFunciones)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private System.Windows.Forms.BindingSource sPOBTENERFUNCIONFECHASBindingSource;
+        private DSFunciones dSFunciones;
+        private DSFuncionesTableAdapters.SP_OBTENER_FUNCION_FECHASTableAdapter sP_OBTENER_FUNCION_FECHASTableAdapter;
+        private System.Windows.Forms.DateTimePicker dtpFechaDesde;
+        private System.Windows.Forms.DateTimePicker dtpFechaHasta;
+        private System.Windows.Forms.Button btnGenerar;
+        private System.Windows.Forms.Label lblDesde;
+        private System.Windows.Forms.Label lblHasta;
+    }
+}
+
---
+++ Reporte Funciones/FrmReporteFunciones.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Reporte_Funciones
+{
+    public partial class FrmReporteFunciones : Form
+    {
+        public FrmReporteFunciones()
+        {
+            InitializeComponent();
+        }
+        public DateTime FechaDesde { get; set; }
+        public DateTime FechaHasta { get; set; }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dtpFechaDesde.Value = DateTime.Today.AddDays(-30);
+            dtpFechaHasta.Value = DateTime.Today;
+           
+        }
+
+        private void btnGenerar_Click(object sender, EventArgs e)
+        {
+            DateTime FechaDesde = dtpFechaDesde.Value;
+            DateTime FechaHasta = dtpFechaHasta.Value;
+            this.sP_OBTENER_FUNCION_FECHASTableAdapter.Fill(dSFunciones.SP_OBTENER_FUNCION_FECHAS, FechaDesde, FechaHasta);
+            this.reportViewer1.RefreshReport();
+        }
+    }
+}
